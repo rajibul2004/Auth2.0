fc9db50dcf992e5fe06f47d91ad0067064425f62
rajibul2004 <rajibulhazari786@gmail.com>
2026-03-12 11:44:12 +0530

    MongoDB connection added.

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ import dotenv from 'dotenv'
 const app = express();
 dotenv.config()
 
+import connectDB from './config/mongodb.js'
+
 const PORT = process.env.PORT || 5000;
 const host = process.env.HOST
 
 app.use(express.json());
+connectDB()
 
 app.get("/", (req, res) => {
-    res.send("Server is running")
+    res.send("Server is running...")
 });
 
 
