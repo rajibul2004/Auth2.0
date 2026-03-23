fc5cafcc00f0ebcf7f960e6b1b9cd8eabffd712e
rajibul2004 <rajibulhazari786@gmail.com>
2026-03-23 19:46:07 +0530

    Final Setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 const app = express();
 dotenv.config()
 
@@ -15,21 +16,28 @@ const allowedOrigin = [process.env.CLIENT_URI,"http://localhost:5173"]
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: allowedOrigin
-    , credentials: true
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
+    origin: [allowedOrigin],
+    credentials: true
+  }));
+}
 connectDB()
 
-app.get("/", (req, res) => {
-    res.send("Server is running...")
-});
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
 app.use('/api/auth',authRotes)
 
+const dir_name=path.resolve();
+const env=process.env.NODE_ENV
+if(env==="production"){
+    app.use(express.static(path.join(dir_name,'../client/dist')));
+    app.get(/.*/,(req,res)=>{
+        res.sendFile(path.join(dir_name,  "../client/dist/index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`App is listening at http://${host}:${PORT}/`)
